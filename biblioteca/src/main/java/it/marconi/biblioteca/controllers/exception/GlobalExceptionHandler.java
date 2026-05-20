e94f4ab6e09e34d467005b8458d6e9d9ad50bf9e
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-05-20 15:37:05 +0200

    feat: Add Docker support and configure application properties for different environments

--- biblioteca/src/main/java/it/marconi/biblioteca/controllers/exception/GlobalExceptionHandler.java
+++ biblioteca/src/main/java/it/marconi/biblioteca/controllers/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package it.marconi.biblioteca.controllers.exception;
 
 import it.marconi.biblioteca.domain.response.APIResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
